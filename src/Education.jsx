1e3be5d69fc199828f14969032af2b6f56bd3952
nkiv <no.kivett@gmail.com>
2025-10-09 17:09:47 -0600

    small change, took away pop from 'Education' section

--- src/Education.jsx
+++ src/Education.jsx
@@ -1,7 +1,7 @@
 function Education() {
   return (
     <div>
-      <h2 className="title pop">Education</h2>
+      <h2 className="title">Education</h2>
       <a href="https://www.colostate.edu/">
         <span className="text-large bold">Colorado State University</span>
       </a>
